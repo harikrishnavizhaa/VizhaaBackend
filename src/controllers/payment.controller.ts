c5f4aacfa0185df131ff58e2768ad540e86c6fd8
harikrishnavizhaa <harikrishnacsbs@gmail.com>
2026-05-07 22:21:39 +0530

    remove backend project files and deployment configurations

--- src/controllers/payment.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export const createOrder = async (_req: Request, res: Response) => {
-    return res.status(501).json({ message: 'Not implemented' });
-};
-
-export const verifyPayment = async (_req: Request, res: Response) => {
-    return res.status(501).json({ message: 'Not implemented' });
-};
